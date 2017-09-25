5c5c6f505588ea390813fa8f3d20638fe25dece6
jahanvi.kuppili <jahanvi.kuppili@CSELAB02-20.bennett.edu.in>
2017-09-25 11:59:06 +0530

    new

---
+++ src/sort/newsort.java
@@ -0,0 +1,24 @@
+package sort;
+
+public class newsort{
+
+	public static void main(String[] args) {
+
+		int ar[]={2,5,1,14,3,76,34,6,9,24};
+		for(int i=0;i<10;i++){
+			if(ar[i]<ar[i+1]){
+				System.out.println("1");
+				int temp=ar[i];
+				ar[i]=temp;
+			}
+			else{
+				System.out.println("2");
+					int temp=ar[i+1];
+					ar[i]=temp;
+			}
+		}
+		System.out.println("Your bubble sort is "+ar);
+		System.out.println(ar[1]);
+	}
+
+}
